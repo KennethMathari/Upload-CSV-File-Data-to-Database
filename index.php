e3d82a9ca85cf61846f08cf65c0220e84789fc37
KennethNdungu <mathari580@gmail.com>
2021-09-14 22:08:36 +0300

    Added Bootstrap 4 for styling

--- index.php
+++ index.php
@@ -23,6 +23,12 @@ if(!empty($_GET['status'])){
     }
 }
 ?>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
 
 <!-- Display status message -->
 <?php if(!empty($statusMsg)){ ?>
