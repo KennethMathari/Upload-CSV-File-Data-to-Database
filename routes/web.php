b31d705d4aeb3eeb05fb3edde0c5e3a06a185cbc
KennethNdungu <mathari580@gmail.com>
2021-09-15 10:09:36 +0300

    Added Index & Upload Routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InventoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [InventoryController::class, 'index'])->name('index');
+Route::post('/', [InventoryController::class, 'upload'])->name('upload');
+
