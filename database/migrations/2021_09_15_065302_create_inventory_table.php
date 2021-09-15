2d0228fe5c03bc91af85c437e6bb0f1becefdb46
KennethNdungu <mathari580@gmail.com>
2021-09-15 10:03:17 +0300

    Created Inventory Table Migration

---
+++ database/migrations/2021_09_15_065302_create_inventory_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateInventoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory', function (Blueprint $table) {
+            $table->integer('InvoiceNo');
+            $table->string('StockCode');
+            $table->string('Description');
+            $table->integer('Quantity');
+            $table->dateTime('InvoiceDate');
+            $table->float('UnitPrice');
+            $table->integer('Customer');
+            $table->string('Country');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory');
+    }
+}
