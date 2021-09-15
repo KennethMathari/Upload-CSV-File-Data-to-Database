7350e5b498dc2c78cd751cecf770d90b8c5f746d
KennethNdungu <mathari580@gmail.com>
2021-09-15 18:38:26 +0300

    Installed TailwindCSS

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Styles -->
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+
     <title>CSV File Upload</title>
 </head>
 <body>
