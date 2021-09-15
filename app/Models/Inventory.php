d28e79b12de89ad41b33994fe52514bd8bf4084b
KennethNdungu <mathari580@gmail.com>
2021-09-15 15:29:19 +0300

    Loop through each pending csv file
    -process one file at a time

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -15,4 +15,17 @@ class Inventory extends Model
     protected $table='inventory';
     protected $fillable = ['invoiceno','stockcode','description','quantity','invoicedate','unitprice','customerid','country'];
     use HasFactory;
+
+    public function importToDB(){
+        //Get CSV File Path
+        $file=resource_path('pendingfiles/*.csv');
+        //Get CSV PendingFiles from resource path
+        $g=glob($file);
+        /**Loop through each pending files.
+        *Process 1 file at a time.
+        */
+        foreach (array_slice($g,0,1) as $file) {
+            dump($file);
+        }
+    }
 }
