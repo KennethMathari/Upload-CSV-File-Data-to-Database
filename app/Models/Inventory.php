f0d40963908a4abfd6a0823bebc13ff2514723da
KennethNdungu <mathari580@gmail.com>
2021-09-15 15:53:31 +0300

    Delete Individual File After Loop Process

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -13,7 +13,7 @@ class Inventory extends Model
      * @var string
      */
     protected $table='inventory';
-    protected $fillable = ['invoiceno','stockcode','description','quantity','invoicedate','unitprice','customerid','country'];
+    protected $guarded=[];
     use HasFactory;
 
     public function importToDB(){
@@ -22,10 +22,28 @@ class Inventory extends Model
         //Get CSV PendingFiles from resource path
         $g=glob($file);
         /**Loop through each pending files.
-        *Process 1 file at a time.
+        * Process 1 file at a time.
         */
         foreach (array_slice($g,0,1) as $file) {
-            dump($file);
+            //Get data from CSV file
+            $data=array_map('str_getcsv', file($file));
+            //Loop through data records
+            foreach ($data as $row) {
+                self::updateOrCreate([
+                    //checks if invoiceno. exists
+                    'InvoiceNo'=>$row[0]
+                ],[
+                    'StockCode'=>$row[1],
+                    'Description'=>$row[2],
+                    'Quantity'=>$row[3],
+                    'InvoiceDate'=>$row[4],
+                    'UnitPrice'=>$row[5],
+                    'Customer'=>$row[6],
+                    'Country'=>$row[7]
+                ]);
+            }
+            //delete file
+            unlink($file);
         }
     }
 }
